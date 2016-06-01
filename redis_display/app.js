4fa9d1e0419777106c8c97fe9e7b105c0a5fcd17
Alex Ellis <alexellis2@gmail.com>
2016-06-01 22:33:34 +0000

    redis display in Node.js

---
+++ redis_display/app.js
@@ -0,0 +1,27 @@
+"use strict"
+
+var redis = require('redis');
+
+var express = require('express');
+var app = express();
+
+var client = redis.createClient({"host": process.env.REDIS_HOST || "redis"});
+client.on('connect', () => {
+   console.log("Connected");
+});
+client.on('error', (err) => {
+   if(err)
+      console.error(err);
+});
+app.get('/', (req,res) => {
+   client.get("node_temp", (err, val) => {
+      if(err) { console.log(err); return res.end(); }
+
+      res.write(val);
+      res.end();
+   });
+});
+
+app.listen(9000, () => {
+   console.log("Listening")
+});
