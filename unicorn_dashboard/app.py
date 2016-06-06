986d757fadaa80056df0b905fdfbd708b1b96c6a
Alex Ellis <alexellis2@gmail.com>
2016-06-06 08:04:49 +0000

    unicorn redis

--- unicorn_dashboard/app.py
+++ unicorn_dashboard/app.py
@@ -1,15 +1,36 @@
 import time
+from reporter import Reporter
+import os
 import unicornhat as UH
-import redis
+print(os.environ)
+#host = os.getenv("REDIS_HOST")
+host = os.environ["REDIS_HOST"]
+print("Host:" + host)
+if(host== None):
+    host = "localhost"
 
-UH.set_pixel( 0, 0,178,34,34)
-UH.show()
-time.sleep(1.5)
-UH.set_pixel( 0, 0,178,255,34)
-UH.show()
-time.sleep(1.5)
-UH.set_pixel( 0, 0,0,255,255)
-UH.show()
-time.sleep(1.5)
 UH.clear()
+UH.show()
 
+r = Reporter(host, "6379")
+
+def on_sensor_data(channel, data):
+    print(channel, data)
+    value = float(r.get_key(data+".temp"))
+    baseline = float(r.get_key(data+".temp.baseline"))
+    print(str(val) + " ~ " + str(baseline))
+    diff = abs(val - baseline)
+
+    if(diff < 1):
+        UH.set_pixel( 0, 0,178,255,34)
+    else:
+        UH.set_pixel( 0, 0,178,34,34)
+
+    UH.show()
+
+r.set_on_sensor_data(on_sensor_data)
+r.subscribe()
+
+while True:
+    print (r.find_members())
+    time.sleep(5)
