9e759ad07c1d08449e7513d1bd24d849b4035eca
Alex <alexellis2@gmail.com>
2016-06-09 09:06:24 +0100

    Add movement to sensor

--- envirophat_sensor/sensing.py
+++ envirophat_sensor/sensing.py
@@ -1,13 +1,16 @@
 import time
 from Adafruit_BME280 import *
+from msense import MotionSense
 
 class Sensors:
     def __init__(self):
         self.sensor = BME280(mode=BME280_OSAMPLE_8)
         self.last_temp = self.read()
+        self.motionSense = MotionSense()
 
     def read(self):
         degrees = self.sensor.read_temperature()
         self.last_temp = degrees
+        self.moving = sense.read()
 
-        return {"temp": degrees, "motion":1}
+        return {"temp": degrees, "motion":self.moving}
