c47ca15419f675a0e10e8f60bc840a28b131d599
Alex <alexellis2@gmail.com>
2016-06-09 09:25:26 +0100

    refactor

--- envirophat_sensor/sensing.py
+++ envirophat_sensor/sensing.py
@@ -5,12 +5,12 @@ from msense import MotionSense
 class Sensors:
     def __init__(self):
         self.sensor = BME280(mode=BME280_OSAMPLE_8)
-        self.last_temp = self.read()
         self.motion_sense = MotionSense()
+
+        self.last = self.read()
 
     def read(self):
         degrees = self.sensor.read_temperature()
-        self.last_temp = degrees
         self.motion = motion_sense.read()
 
         return {"temp": degrees, "motion":self.motion}
