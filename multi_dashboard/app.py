c5b8293b85f6853f8568d89c05a5101c3836a56a
Alex Ellis <alexellis2@gmail.com>
2016-06-10 10:34:06 +0100

    painter

--- multi_dashboard/app.py
+++ multi_dashboard/app.py
@@ -38,28 +38,29 @@ def paint():
 
 def on_sensor_data(channel, data):
     print(channel, data)
-    motion = r.get_key(data+".motion")
-    if(motion != None and float(motion) > 0):
-        on(0,0,255)
-        UH.show()
-        return
+    paint()
+    # motion = r.get_key(data+".motion")
+    # if(motion != None and float(motion) > 0):
+    #     on(0,0,255)
+    #     UH.show()
+    #     return
 
-    value = r.get_key(data+".temp")
-    baseline = r.get_key(data+".temp.baseline")
-    print(baseline,value)
-    if(baseline != None and value != None):
-        baseline = round(float(baseline), 2)
-        value = round(float(value), 2)
+    # value = r.get_key(data+".temp")
+    # baseline = r.get_key(data+".temp.baseline")
+    # print(baseline,value)
+    # if(baseline != None and value != None):
+    #     baseline = round(float(baseline), 2)
+    #     value = round(float(value), 2)
 
-        diff = abs(value - baseline)
-        print(str(value) + " ~ " + str(baseline) + " = " + str(diff))
+    #     diff = abs(value - baseline)
+    #     print(str(value) + " ~ " + str(baseline) + " = " + str(diff))
 
-        if(diff < 1.5):
-            on(0,255,0)
-        else:
-            on(255,0,0)
+    #     if(diff < 1.5):
+    #         on(0,255,0)
+    #     else:
+    #         on(255,0,0)
 
-        UH.show()
+    #     UH.show()
 
 
 r.set_on_sensor_data(on_sensor_data)
